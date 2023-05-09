05d39cb39b607853fd441cf6491e1b2c33dfb768
Renan Teixeira <renan_replay@live.com>
2023-05-09 11:32:54 -0300

    adicionar video na pagina de sobre

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image';
 import { FaInstagram, FaLinkedin, FaMailBulk, FaWhatsapp } from 'react-icons/fa';
-import profile from '../../../public/profile.jpg';
 
 function SocialLink({ href, children, icon: Icon }: any) {
   return (
@@ -19,13 +17,11 @@ export default function Sobre() {
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
         <div className="lg:pl-20">
           <div className="max-w-xs px-2.5 lg:max-w-none">
-            <Image
-              src={profile}
-              width={400}
-              height={400}
-              alt=""
-              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-            />
+
+            <video autoPlay loop className='aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800'>
+              <source src="/video.mp4" />
+            </video>
+
           </div>
         </div>
         <div className="lg:order-first lg:row-span-2">
