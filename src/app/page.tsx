953d60656bd5a182af9a39ddcbe2d5af9e4b100f
Renan Teixeira <renan_replay@live.com>
2023-05-10 18:40:39 -0300

    ajuste na de titulos e descricoes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Page() {
       <div className="flex flex-col  md:items-start items-center">
 
         <div>
-          <h1 className='text-4xl mb-2'>Psicóloga Infantil e Juvenil</h1>
+          <h1 className='text-4xl mb-2'>Psicóloga Infantil</h1>
         </div>
         <div className='flex flex-row '>
-          <h5 className='md:basis-10/12 md:text-lg text-sm font-thin'>Especializada em crianças e adolescentes, com formação em Análise do Comportamento aplicada ao Transtorno do Espectro Autista e Deficiência pela CBI of Miami. Atendo em Jundiaí e região, oferecendo acompanhamento psicológico infantil-juvenil e orientação para pais.</h5>
+          <h5 className='md:basis-10/12 md:text-lg text-sm font-thin'>Especializada em crianças e adolescentes, com formação em Análise do Comportamento aplicada ao Transtorno do Espectro Autista e Deficiência pela CBI of Miami. Atendo em Jundiaí e região, oferecendo acompanhamento psicológico infanto-juvenil e orientação para pais.</h5>
         </div>
         <div className='flex flex-row mt-2'>
           <a className='mr-2' href="https://api.whatsapp.com/send?phone=5511914175676"
